78f1574aa1e739d230ec66f2549d89d881241da2
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-12-02 15:42:02 +0530

    Add help subcommand

--- bin/dog.dart
+++ bin/dog.dart
@@ -1,5 +1,55 @@
-import 'package:dog/dog.dart' as dog;
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:args/args.dart';
+
+const lineNumber = 'line-number';
+const help = 'help';
 
 void main(List<String> arguments) {
-  print('Hello world: ${dog.calculate()}!');
+  exitCode = 0; // presume success
+  final parser = ArgParser();
+  parser.addFlag(lineNumber, negatable: false, abbr: 'n');
+  parser.addFlag(help, negatable: false, abbr: 'h');
+
+  ArgResults argResults = parser.parse(arguments);
+  final paths = argResults.rest;
+
+  if (argResults[help]) {
+    print("This is a cat clone written in dart.\nUse -n to show line numbers");
+  } else {
+    dog(paths, showLineNumbers: argResults[lineNumber] as bool);
+  }
+}
+
+Future<void> dog(List<String> paths, {bool showLineNumbers = false}) async {
+  if (paths.isEmpty) {
+    // No files provided as arguments. Read from stdin and print each line.
+    await stdin.pipe(stdout);
+  } else {
+    for (final path in paths) {
+      var lineNumber = 1;
+      final lines = utf8.decoder
+          .bind(File(path).openRead())
+          .transform(const LineSplitter());
+      try {
+        await for (final line in lines) {
+          if (showLineNumbers) {
+            stdout.write('${lineNumber++} ');
+          }
+          stdout.writeln(line);
+        }
+      } catch (_) {
+        await _handleError(path);
+      }
+    }
+  }
+}
+
+Future<void> _handleError(String path) async {
+  if (await FileSystemEntity.isDirectory(path)) {
+    stderr.writeln('error: $path is a directory');
+  } else {
+    exitCode = 2;
+  }
 }
